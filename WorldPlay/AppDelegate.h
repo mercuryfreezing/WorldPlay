0a4ca80518a105cf8f790a29e5ed684c9b948522
roshan <roshan.prakash.r@gmail.com>
2014-09-30 21:00:49 +0100

    Initial Commit

---
+++ WorldPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorldPlay
+//
+//  Created by roshan on 30/09/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WorldPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorldPlay
+//
+//  Created by roshan on 30/09/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
